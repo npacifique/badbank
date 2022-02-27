a0c321e30ec7d3bcbda5e1999bf8fb0706324efc
Pacifique N. Ntahurunga <pacifique@npacifique.local>
2022-02-27 11:25:17 -0500

    add users

---
+++ server/data/sampleData.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+        "id":"xxx",
+        "firstName":"Joe",
+        "LastName":"Doe",
+        "username" : "joe.doe",
+        "emailAddress" : "joe.doe@gmail.com", 
+        "password" : "pasNow", 
+        "account" : 123, 
+        "isAdmin":false,
+        "deposits" : 
+        [
+            "02/27/2022" = [
+                {"time" : "10:00 AM", "amount" : 10}
+            ]
+
+        ], 
+        "widthrows" : 
+        [
+            "02/27/2022" = [
+                {"time" : "10:05 AM", "amount" : 5},
+                {"time" : "10:06 AM", "amount" : 5}
+            ]  
+        ]
+    }, 
+    {
+        "id":"xxx",
+        "firstName":"Ann",
+        "LastName":"Beth",
+        "username" : "ann.beth",
+        "emailAddress" : "ann.beth@gmail.com", 
+        "password" : "pasNow", 
+        "account" : 123, 
+        "isAdmin":false,
+        "deposits" : 
+        [
+            "02/27/2022" = [
+                {"time" : "11:00 AM", "amount" : 110}
+            ]
+
+        ], 
+        "widthrows" : 
+        [
+            "02/27/2022" = [
+                {"time" : "12:05 PM", "amount" : 5},
+                {"time" : "14:06 pM", "amount" : 5}
+            ]  
+        ]
+    }
+]
