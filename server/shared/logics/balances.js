0f6d190c0cddde760d46d974d0151ec130fa6afe
Pacifique N. Ntahurunga <pacifique@npacifique.local>
2022-02-27 12:59:46 -0500

    add getWidthrowTotal

--- server/shared/logics/balances.js
+++ server/shared/logics/balances.js
@@ -3,27 +3,56 @@ const users = require('../../data/sampleData')
 //console.log(users["joe.doe"].deposits)
 
 
-
-
-
-
-
 //console.log(users["joe.doe"].widthrows)
-const getWidthrowTotal = (username)=>{
 
-    const keys = Object.keys(users[username].widthrows)
 
-    const user = users[username].widthrows
-
-    let total = 0
+class Calc {
     
-    keys.forEach(k => {
+    constructor(username){
+        this.username = username
+    }
+    
+    getWidthrowTotal(username){
 
-        user[k].forEach(d => {
-            console.log(d.amount)
+        const keys = Object.keys(users[username].widthrows)
+    
+        const user = users[username].widthrows
+    
+        let total = 0
+        
+        keys.forEach(k => {
+    
+            user[k].forEach(d => {
+                //console.log(d.amount)
+                total+=d.amount
+            });
         });
-    });
+        
+    }
+
+
+    getDepositsTotal(username){
+
+        const keys = Object.keys(users[username].deposits)
     
+        const user = users[username].deposits
+    
+        let total = 0
+        
+        keys.forEach(k => {
+    
+            user[k].forEach(d => {
+                //console.log(d.amount)
+                total+=d.amount
+            });
+        });
+        
+    }
+
 }
 
-getWidthrowTotal("joe.doe")
+var calc = new Calc()
+
+
+calc.getDepositsTotal("joe.doe")
+
